1ee39f39f89f944aa7c7352d4645cd30bbf73266
Santiago <santiagocabrera354@gmail.com>
2023-07-16 15:31:56 -0500

    agregando cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
+
+  {path: "", redirectTo:"main", pathMatch:"full"},
+  {path: "main", component:  MainComponent},
+
   {path: "registrarse", component:  RegisterComponent},
   {path: "login", component:  LoginComponent},
+
+
 ];
 
 @NgModule({
